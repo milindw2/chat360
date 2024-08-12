73dcd8aa497f5dffdd6b90b047703a3073adc493
Milind Waghmare <milind.waghmare@recity.in>
2024-08-12 11:21:21 +0530

    minor refactoring

--- src/Components/data-fetch/searchData.js
+++ src/Components/data-fetch/searchData.js
@@ -2,13 +2,17 @@ import { useEffect, useState } from "react"
 
 const useUserSearchData = ()=>{
     // const [searchResult, setSearchResult] = useState([])
+    
 const search =    async (searchString)=>{
-    const res =await fetch(`https://dummyjson.com/users/search?q=${searchString}`)
-    const result = await res.json();
-    // console.log("result",result.users)
-    // setSearchResult(result.users)
-    // setCount(result.total)
+    try {
+        const res =await fetch(`https://dummyjson.com/users/search?q=${searchString}`)
+        const result = await res.json();
+   
     return {users:result.users, count: result.total }
+    } catch (error) {
+        console.log(error)
+    }
+    
 }
 return {search}
 }
--- src/Components/data-fetch/userData.js
+++ src/Components/data-fetch/userData.js
@@ -1,20 +1,31 @@
 import { useEffect, useState } from "react"
 
 const useUserData = ({setCount, limit, skip})=>{
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
     const [usersList, setUsersList] = useState([])
     const [userCount, setUserCount]= useState(0)
-    
+
 useEffect(()=>{
-    (async ()=>{
-        const res =await fetch(`https://dummyjson.com/user?skip=${skip}&limit=${limit}`)
-        const result = await res.json();
-        // console.log("result in user data", result)
-        setUsersList(result?.users)
-        setUserCount(result.total)
-    })()
+    try {
+        setLoading(true)
+        (async ()=>{
+            const res =await fetch(`https://dummyjson.com/user?skip=${skip}&limit=${limit}`)
+           
+            const result = await res.json();
+            setUsersList(result?.users)
+            setUserCount(result.total)
+            setLoading(false)
+        })()
+    } catch (error) {
+        setError(JSON.stringify(error))
+        console.log("error", error)
+        setLoading(false)
+    }
+   
 },[limit, skip])
 // console.log("usersList", usersList)
-return {usersList, userCount}
+return {usersList, userCount, loading, error}
 }
 
 export default useUserData
